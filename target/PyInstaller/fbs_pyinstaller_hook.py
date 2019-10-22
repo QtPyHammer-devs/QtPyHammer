b9e87de91113fba513915404f4f925c2bbef58b5
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2019-10-23 07:43:41 +1000

    deleted target (local build) folder

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'QtPyHammer', 'author': 'snake-biscuits', 'version': '0.0.0', 'environment': 'local'}
