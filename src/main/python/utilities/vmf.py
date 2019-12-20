792b7a455bba33e3490783b1c197f999060b33f4
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2019-12-20 21:41:24 +1000

    copied updates to vmf_tool which remove XSS threat posed by relying on exec() & eval()

--- src/main/python/utilities/vmf.py
+++ src/main/python/utilities/vmf.py
@@ -1,8 +1,7 @@
 """https://github.com/snake-biscuits/vmf_tool"""
-## e.g. "visgroupid" "7"\n"visgroupid" "8" = {'visgroupid': ['7', '8']}
-#TODO: Functions for handling the horrible visgroup system
-import io
-import textwrap
+# TODO: Functions for handling the horrible visgroup system
+# e.g. "visgroupid" "7"\n"visgroupid" "8" = {'visgroupid': ['7', '8']}
+
 
 def pluralise(word):
     if word.endswith('f'): # self -> selves
@@ -14,6 +13,7 @@ def pluralise(word):
     else: # side -> sides
         return word + 's'
 
+
 def singularise(word):
     if word.endswith('ves'): # self <- selves
         return word[:-3] + 'f'
@@ -21,95 +21,14 @@ def singularise(word):
         return word[:-3] + 'y'
     elif word.endswith('ices'): # vertex <- vertices
         return word[:-4] + 'ex'
-    elif word.endswith('s'): # side <-  sides
+    # in the face of ambiguity, refuse the temptation to guess
+    elif word.endswith('s'): # side <- sides
         return word[:-1]
-    else: # in the face of ambiguity, refuse the temptation to guess
+    else:
         return word # assume word is already singular
 
-class scope:
-    """Handles a string used to index a multi-dimensional dictionary, correctly reducing nested lists of dictionaries"""
-    def __init__(self, strings=[]):
-        self.strings = strings
 
-    def __len__(self):
-        """Returns depth, ignoring plural indexes"""
-        return len(filter(lambda x: isinstance(x, str), self.strings))
-
-    def __repr__(self):
-        """Returns scope as a string that can index a deep dictionary"""
-        scope_string = ''
-        for string in self.strings:
-            if isinstance(string, str):
-                scope_string += "['{}']".format(string)
-            elif isinstance(string, int):
-                scope_string += "[{}]".format(string)
-        return scope_string
-
-    def add(self, new):
-        self.strings.append(new)
-
-    def reduce(self, count):
-        for i in range(count):
-            try:
-                if isinstance(self.strings[-1], int):
-                    self.strings = self.strings[:-2]
-                else:
-                    self.strings = self.strings[:-1]
-            except:
-                break
-
-
-def namespace_from(text_file):
-    """String or .vmf file to nested namespace"""
-    if isinstance(text_file, io.TextIOWrapper):
-        file_iter = text_file.readlines()
-    elif isinstance(text_file, str):
-        file_iter = text_file.split('\n')
-    else:
-        raise RuntimeError("Cannot construct dictionary from {}!".format(type(text_file)))
-    namespace_nest = namespace({})
-    current_scope = scope([])
-    previous_line = ''
-    for line_number, line in enumerate(file_iter):
-        try:
-            new_namespace = namespace({'_line': line_number})
-            line = line.rstrip('\n')
-            line = textwrap.shorten(line, width=2000) # cleanup spacing, broke at 200+ chars, not the right tool for the job
-            if line == '' or line.startswith('//'): # ignore blank / comments
-                continue
-            elif line =='{': # START declaration
-                current_keys = eval("namespace_nest{}.__dict__.keys()".format(current_scope))
-                plural = pluralise(previous_line)
-                if previous_line in current_keys: # NEW plural
-                    exec("namespace_nest{}[plural] = [namespace_nest{}[previous_line]]".format(current_scope, current_scope))
-                    exec("namespace_nest{}.__dict__.pop(previous_line)".format(current_scope))
-                    exec("namespace_nest{}[plural].append(new_namespace)".format(current_scope))
-                    current_scope = scope([*current_scope.strings, plural, 1]) # why isn't this a method?
-                elif plural in current_keys: # APPEND plural
-                    current_scope.add(plural)
-                    exec("namespace_nest{}.append(new_namespace)".format(current_scope))
-                    current_scope.add(len(eval("namespace_nest{}".format(current_scope))) - 1)
-                else: # NEW singular
-                    current_scope.add(previous_line)
-                    exec("namespace_nest{} = new_namespace".format(current_scope))
-            elif line == '}': # END declaration
-                current_scope.reduce(1)
-            elif '" "' in line: # KEY VALUE
-                key, value = line.split('" "')
-                key = key.lstrip('"')
-                value = value.rstrip('"')
-                exec("namespace_nest{}[key] = value".format(current_scope))
-            elif line.count(' ') == 1:
-                key, value = line.split()
-                exec("namespace_nest{}[key] = value".format(current_scope))
-            previous_line = line.strip('"')
-        except Exception as exc:
-            print("error on line {0:04d}:\n{1}\n{2}".format(line_number, line, previous_line))
-            raise exc
-    return namespace_nest
-
-
-class namespace: # DUNDER METHODS ONLY!
+class namespace: # VMF COULD OVERLOAD METHODS
     """Nested Dicts -> Nested Objects"""
     def __init__(self, nested_dict=dict()):
         for key, value in nested_dict.items() if isinstance(nested_dict, dict) else nested_dict.__dict__.items():
@@ -127,39 +46,137 @@ class namespace: # DUNDER METHODS ONLY!
         return self.__dict__[str(index)]
 
     def __iter__(self):
-        return iter(self.__dict__)
+        return iter(self.__dict__.keys())
 
     def __len__(self):
         return len(self.__dict__.keys())
 
     def __repr__(self):
-        attrs = [a if ' ' not in a else '"{}"'.format(a) for a in self.__dict__.keys()]
-        return "namespace([{}])".format(", ".join(attrs))
+        attrs = []
+        for attr_name, attr in self.items():
+            if " " in attr_name:
+                attr_name = "{}".format(attr_name)
+            attr_string = "{}: {}".format(attr_name, attr.__class__.__name__)
+            attrs.append(attr_string)
+        return "<namespace({})>".format(", ".join(attrs))
 
-    def items(self): # fix for lines_from
+    def items(self):
         for k, v in self.__dict__.items():
             yield (k, v)
 
 
-def dict_from(namespace_nest):
-    out = dict()
-    for key, value in namespace_nest.__dict__.items():
-        if isinstance(value, namespace):
-            out[key] = dict_from(value)
-        elif isinstance(value, list):
-            out[key] = [dict_from(i) for i in value]
-        else:
-            out[key] = value
-    return out
+class scope:
+    """Array of indices into a nested array"""
+    def __init__(self, tiers=[]):
+        self.tiers = tiers
+
+    def __repr__(self):
+        repr_strings = []
+        for tier in self.tiers:
+            if isinstance(tier, str):
+                if " " in tier or tier[0].lower() not in "abcdefghijklmnopqrstuvwxyz":
+                    repr_strings.append("['{}']".format(tier))
+                else:
+                    repr_strings.append(".{}".format(tier))
+            else:
+                repr_strings.append("[{}]".format(tier))
+        return "".join(repr_string)
+
+    def add(self, tier):
+        """Go a layer deeper"""
+        self.tiers.append(tier)
+
+    def increment(self): # does not check if tiers[-1] is an integer
+        self.tiers[-1] += 1
+
+    def get_from(self, nest):
+        """Gets the item stored in nest which this scope points at"""
+        target = nest
+        for tier in self.tiers:
+            target = target[tier]
+        return target
+
+    def retreat(self):
+        """Retreat up 1 tier (2 tiers for plurals)"""
+        popped = self.tiers.pop(-1)
+        if isinstance(popped, int):
+            self.tiers.pop(-1)
+
+    def remove_from(self, nest): # used for changing singulars into plurals
+        """Delete the item stored in nest which this scope points at"""
+        target = nest
+        for i, tier in enumerate(self.tiers):
+            if i == len(self.tiers) - 1: # must set from tier above
+                target.pop(tier)
+            else:
+                target = target[tier]
+        # stop pointing at deleted index
+        self.retreat()
+        # unsure what will happen when deleting an item from a plural
+        # also unsre what SHOULD happen when deleting an item from a plural
+
+    def set_in(self, nest, value):
+        """Sets the item stored in nest which this scope points at to value"""
+        target = nest
+        for i, tier in enumerate(self.tiers):
+            if i == len(self.tiers) - 1: # must set from tier above
+                target[tier] = value # could be creating this value
+            else:
+                target = target[tier]
+    
+
+def parse_lines(iterable):
+    """String or .vmf file to nested namespace"""
+    nest = namespace({})
+    current_scope = scope([])
+    previous_line = ''
+    for line_number, line in enumerate(iterable):
+        try:
+            new_namespace = namespace({'_line': line_number})
+            current_target = current_scope.get_from(nest)
+            line = line.strip()  # cleanup spacing
+            if line == "" or line.startswith("//"): # ignore blank / comments
+                continue
+            elif line =="{": # START declaration
+                current_keys = current_target.__dict__.keys()
+                plural = pluralise(previous_line)
+                if previous_line in current_keys: # NEW plural
+                    current_target[plural] = [current_target[previous_line]] # create plural from old singular
+                    current_target.__dict__.pop(previous_line) # delete singular
+                    current_target[plural].append(new_namespace) # second entry
+                    current_scope.add(plural)
+                    current_scope.add(1) # point at new_namespace
+                elif plural in current_keys: # APPEND plural
+                    current_scope.add(plural) # point at plural
+                    current_scope.get_from(nest).append(new_namespace)
+                    current_scope.add(len(current_scope.get_from(nest)) - 1) # current index in plural
+                else: # NEW singular
+                    current_scope.add(previous_line)
+                    current_scope.set_in(nest, new_namespace)
+            elif line == '}': # END declaration
+                current_scope.retreat()
+            elif '" "' in line: # KEY VALUE
+                key, value = line.split('" "')
+                key = key.lstrip('"')
+                value = value.rstrip('"')
+                current_target[key] = value
+            elif line.count(' ') == 1:
+                key, value = line.split()
+                current_target[key] = value
+            previous_line = line.strip('"')
+        except Exception as exc:
+            print("error on line {0:04d}:\n{1}\n{2}".format(line_number, previous_line, line))
+            raise exc
+    return nest
 
 
 def lines_from(_dict, tab_depth=0): # rethink & refactor
-    '''Takes a nested dictionary (which may also contain lists, but not tuples)
+    """Takes a nested dictionary (which may also contain lists, but not tuples)
 from this a series of strings resembling valve's text format used in .vmf files
-are generated approximately one line at a time'''
+are generated approximately one line at a time"""
     tabs = '\t' * tab_depth
     for key, value in _dict.items():
-        if isinstance(value, (dict, namespace)): # another layer
+        if isinstance(value, (dict, namespace)): # another nest
             value = (value,)
         elif isinstance(value, list): # collection of plurals
             key = singularise(key)
@@ -169,16 +186,17 @@ are generated approximately one line at a time'''
             yield """{}"{}" "{}"\n""".format(tabs, key, value)
             continue
         for item in value:
-            yield """{}{}\n{}""".format(tabs, key, tabs) + "{\n" # open into the next layer
+            yield """{}{}\n{}""".format(tabs, key, tabs) + "{\n" # go deeper
             for line in lines_from(item, tab_depth + 1): # recurse down
                 yield line
     if tab_depth > 0:
-        yield "\t" * (tab_depth - 1) + "}\n" # close the layer
+        yield "\t" * (tab_depth - 1) + "}\n" # close the plural index / namespace
 
 
-def export(_dict, outfile):
+def export(nest, outfile):
     """Don't forget to close the file afterwards!"""
     print("Exporting {} ... ".format(outfile.name), end="")
-    for line in lines_from(_dict): # using a buffer to write in chunks may be wise
-        outfile.write(line) # ''.join([line for line in lines_from(_dict)]) also works
+    for line in lines_from(nest):
+        outfile.write(line) # writing one line at a time seems wasteful
+        # ''.join([line for line in lines_from(_dict)]) also works
     print("Done!")
