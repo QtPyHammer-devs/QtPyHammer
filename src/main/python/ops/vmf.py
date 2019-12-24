a2c43609a7211572b8cb2baab3ece97b45fccdf8
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2019-12-24 13:07:55 +1000

    updated ops/vmf to work with new function name

--- src/main/python/ops/vmf.py
+++ src/main/python/ops/vmf.py
@@ -4,36 +4,34 @@ sys.path.insert(0, "../") # sibling packages
 # import ops.timeline as timeline
 # from utilities import entity
 from utilities import solid
-from utilities.vmf import namespace_from
+from utilities.vmf import parse_lines
 
 
 class interface:
     def __init__(self, parent, vmf_file):
         self.parent = parent # to update the MapTab's render manager
-        self.source_vmf = namespace_from(vmf_file) # len(lines) / i -> progress
+        self.log = []
+        self.source_vmf = parse_lines(vmf_file.readlines())
         self.skybox = self.source_vmf.world.skyname
         self.detail_material = self.source_vmf.world.detailmaterial
         self.detail_vbsp = self.source_vmf.world.detailvbsp
+        # see utilities.vmf for new approach to loading from text files
         raw_brushes = []
         if hasattr(self.source_vmf.world, "solids"):
             raw_brushes = self.source_vmf.world.solids
         elif hasattr(self.source_vmf.world, "solid"):
             raw_brushes.append(self.source_vmf.world.solid)
-        self.brushes = [] # len() / i -> progress
+        self.brushes = []
         for i, brush in enumerate(raw_brushes):
             try:
                 valid_solid = solid.load(brush)
             except Exception as exc:
                 report = "Solid #{} id: {} is invalid.\n{}".format(i, brush.id, exc)
-                self.import_log.append(report)
+                self.log.append(report)
             else:
                 self.brushes.append(valid_solid)
-        # self.entities = []
-        # if hasattr(vmf, "entities"):
-        #     self.entities = vmf.world.entities
-        # elif hasattr(vmf, "entitiy"):
-        #     self.entities.append(vmf.world.entity)
-        # self.entities = [entity.import(e) for e in self.entities] # len() / i -> progress
+        if len(self.log) > 0:
+            print(*self.log, sep="\n")
 
     def add_brushes(self, *brushes):
         # self.parent.edit_timeline.add(timeline.op.BRUSH_ADD, brushes)
