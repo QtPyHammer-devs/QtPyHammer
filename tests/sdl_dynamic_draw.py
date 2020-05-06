964d31694aeb85a775ba69a1ae93d3db6792feab
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-05-06 21:28:33 +1000

    added GLES support and testing without SubData (shaders? not working)

--- tests/sdl_dynamic_draw.py
+++ tests/sdl_dynamic_draw.py
@@ -13,7 +13,7 @@ from sdl2 import *
 
 def main(width, height):
     SDL_Init(SDL_INIT_VIDEO)
-    window = SDL_CreateWindow(b'SDL2 OpenGL', SDL_WINDOWPOS_CENTERED,  SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS)
+    window = SDL_CreateWindow(b"SDL2 OpenGL", SDL_WINDOWPOS_CENTERED,  SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS)
     glContext = SDL_GL_CreateContext(window)
     SDL_GL_SetSwapInterval(0)
     glClearColor(0.1, 0.1, 0.1, 0.0)
@@ -32,15 +32,16 @@ def main(width, height):
     # VERTEX
     VERTEX_BUFFER = glGenBuffers(1)
     glBindBuffer(GL_ARRAY_BUFFER, VERTEX_BUFFER)
-    glBufferData(GL_ARRAY_BUFFER, 256, None, GL_DYNAMIC_DRAW)
+##    glBufferData(GL_ARRAY_BUFFER, 256, None, GL_DYNAMIC_DRAW)
     # ^ target, size, *data, usage
     
     cube_vertices = [(-1, 1, 1), (1, 1, 1), (1, -1, 1), (-1, -1, 1),
                      (-1, 1, -1), (1, 1, -1), (1, -1, -1), (-1, -1, -1)]
     cube_vertices = [*itertools.chain(*cube_vertices)]
     cube_vertices = np.array(cube_vertices, dtype=np.float32)
-    glBufferSubData(GL_ARRAY_BUFFER, 0, len(cube_vertices) * 4, cube_vertices)
+##    glBufferSubData(GL_ARRAY_BUFFER, 0, len(cube_vertices) * 4, cube_vertices)
     # ^ target, start, length, *data
+    glBufferData(GL_ARRAY_BUFFER, 256, cube_vertices, GL_DYNAMIC_DRAW)
 
     # CHECK
     vertex_buffer_data = glGetBufferSubData(GL_ARRAY_BUFFER, 0, 8 * 3 * 4)
@@ -51,13 +52,14 @@ def main(width, height):
     # INDEX
     INDEX_BUFFER = glGenBuffers(1)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, INDEX_BUFFER)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 256, None, GL_DYNAMIC_DRAW)
+##    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 256, None, GL_DYNAMIC_DRAW)
     # ^ target, size, *data, usage
 
     cube_indices = [0, 1, 2, 3, 4, 5, 6, 7]
     cube_indices = np.array(cube_indices, dtype=np.uint32)
-    glBufferSubData(GL_ARRAY_BUFFER, 0, len(cube_indices) * 4, cube_indices)
+##    glBufferSubData(GL_ARRAY_BUFFER, 0, len(cube_indices) * 4, cube_indices)
     # ^ target, start, length, *data
+    glBufferData(GL_ARRAY_BUFFER, 256, cube_indices, GL_DYNAMIC_DRAW)
 
     # CHECK
     index_buffer_data = glGetBufferSubData(GL_ARRAY_BUFFER, 0, 8 * 4)
@@ -66,7 +68,12 @@ def main(width, height):
     print(indices)
     ###  END DYNAMIC DRAW BUFFER TEST  ###
 
-    vertex_shader_source = """#version 450 core
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 12, GLvoidp(0))
+
+    ###  BEGIN SHADERS  ###
+    # VERT SHADER SOURCE
+    vertex_shader_core_source = """#version 450 core
     layout(location = 0) in vec3 vertex_position;
     uniform mat4 gl_ModelViewProjectionMatrix;
     out vec3 position;
@@ -74,30 +81,64 @@ def main(width, height):
     void main()
     {
         position = vertex_position;
-
         gl_Position = gl_ModelViewProjectionMatrix * vec4(vertex_position, 1);
     }
     """
-    vertex_shader = compileShader(vertex_shader_source, GL_VERTEX_SHADER)
+    vertex_shader_gles_source = """#version 300 es
+    layout(location = 0) in vec3 vertex_position;
+    uniform mat4 ModelViewProjectionMatrix;
+    out vec3 position;
     
-    fragment_shader_source = """#version 450 core
+    void main()
+    {
+        position = vertex_position;
+        gl_Position = ModelViewProjectionMatrix * vec4(vertex_position, 1);
+    }
+    """
+    
+    # FRAG SHADER SOURCE
+    fragment_shader_core_source = """#version 450 core
     layout(location = 0) out vec4 outColour;
     in vec3 position;
 
     void main()
     {
         vec4 Ka = vec4(0.15, 0.15, 0.15, 1);
-        outColour = vec4(.5, .5, .5, 1) + Ka;
+        outColour = vec4(.75, .75, .75, 1) + Ka;
     }
     """
-    fragment_shader = compileShader(fragment_shader_source, GL_FRAGMENT_SHADER)
+    fragment_shader_gles_source = """#version 300 es
+    layout(location = 0) out mediump vec4 outColour;
+    in mediump vec3 position;
 
-    shader_program = compileProgram(vertex_shader, fragment_shader)
+    void main()
+    {
+        mediump vec4 Ka = vec4(0.15, 0.15, 0.15, 1);
+        outColour = vec4(.75, .75, .75, 1) + Ka;
+    }
+    """
+
+    major = glGetIntegerv(GL_MAJOR_VERSION)
+    minor = glGetIntegerv(GL_MINOR_VERSION)
+
+    if major >= 4 and minor >= 5:
+        GLES = False
+        vert_source = vertex_shader_core_source
+        frag_source = fragment_shader_core_source
+    else:
+        GLES = True
+        vert_source = vertex_shader_gles_source
+        frag_source = fragment_shader_gles_source
+    
+    vert_shader = compileShader(vert_source, GL_VERTEX_SHADER)
+    frag_shader = compileShader(frag_source, GL_FRAGMENT_SHADER)
+
+    shader_program = compileProgram(vert_shader, frag_shader)
     glLinkProgram(shader_program)
     glUseProgram(shader_program)
-
-    glEnableVertexAttribArray(0)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 12, GLvoidp(0))
+    if GLES:
+        location = glGetUniformLocation(shader_program, "ModelViewProjectionMatrix")
+    ###  END SHADERS  ###
     
     tickrate = 1 / 0.015
     old_time = time.time()
@@ -114,6 +155,10 @@ def main(width, height):
         while dt >= 1 / tickrate:
             # do logic for frame
             glRotate(6 / tickrate, 1, 0, 1.25)
+            if GLES:
+                matrix = glGetFloatv(GL_PROJECTION_MATRIX)
+                glUseProgram(shader_program)
+                glUniformMatrix4fv(location, 1, GL_FALSE, matrix)
             # end frame
             dt -= 1 / tickrate
             old_time = time.time()
@@ -121,16 +166,24 @@ def main(width, height):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         # BEGIN DRAW
         glUseProgram(shader_program)
+        glDrawArrays(GL_POINTS, 0, 8)
         glDrawElements(GL_POINTS, 8, GL_UNSIGNED_INT, GLvoidp(0))
         
-##        glUseProgram(0)
-##        glColor(1, 0, 1)
-##        glBegin(GL_POINTS)
-##        glVertex(0, 0, 0)
-##        glVertex(1, 0, 0)
-##        glVertex(0, 1, 0)
-##        glVertex(0, 0, 1)
-##        glEnd()
+        glUseProgram(0)
+        glColor(0, .5, .5)
+        glBegin(GL_LINE_LOOP)
+        glVertex(0, 0, 0)
+        glVertex(1, 0, 0)
+        glVertex(0, 1, 0)
+        glVertex(0, 0, 1)
+        glEnd()
+        glColor(1, 0, 1)
+        glBegin(GL_POINTS)
+        glVertex(0, 0, 0)
+        glVertex(1, 0, 0)
+        glVertex(0, 1, 0)
+        glVertex(0, 0, 1)
+        glEnd()
         
         # END DRAW
         SDL_GL_SwapWindow(window)
