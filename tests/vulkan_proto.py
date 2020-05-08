88735a0cad412090952532ec3fa2c03cecf53a56
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-05-08 11:49:50 +1000

    working on window surface
    PyQt5 vulkan support is incomplete so I'm executing C via cffi instead of creating a surface the Qt way (the .create() method sucks)

--- tests/vulkan_proto.py
+++ tests/vulkan_proto.py
@@ -1,4 +1,6 @@
+import sys
 # Third-party
+from cffi import FFI
 from PyQt5 import QtGui, QtWidgets
 from vulkan import *
 
@@ -22,7 +24,7 @@ class VulkanWindow(QtWidgets.QWidget):
         
         self.instance_info = VkInstanceCreateInfo(
             pApplicationInfo = self.app_info,
-            enabledLayerCount = 0,
+            enabledLayerCount = 0, # validation layers
             enabledExtensionCount = len(self.extensions),
             ppEnabledExtensionNames = self.extensions)
         
@@ -30,43 +32,67 @@ class VulkanWindow(QtWidgets.QWidget):
         if self._instance == None:
             raise RuntimeError("Could not create Vulkan instance")
 
-        # physical device selection
-        self._physical_device = None
-        physical_devices = vkEnumeratePhysicalDevices(self._instance)
-        # a filter function would probably be more pythonic here
-        for device in physical_devices:
-            family = vkGetPhysicalDeviceQueueFamilyProperties(device)
-            for graphics_family, prop in enumerate(family):
-                if prop.queueCount > 0:
-                    if prop.queueFlags & VK_QUEUE_GRAPHICS_BIT:
-                        self._physical_device = device
-                        break
-        # seems like a good place for a 'for: else:', but inverted somehow?
-        if self._physical_device == None:
-            raise RuntimeError("Could not find suitable physical device")
-        # ^^^ FILTHY DIRTY ^^^ (far from pythonic)
+        # DEBUG CALLBACK GOES HERE
 
-        q_create_info = VkDeviceQueueCreateInfo(
-            queueFamilyIndex = graphics_family, # collected above
-            queueCount = 1,
-            pQueuePriorities = [1.0])
+        # surface creation
+        if sys.platform == "win32":
+            c_intf = FFI() # get the hInstance of the current window
+            c_intf.cdef("long __stdcall GetWindowLongA(void* hWnd, int nIndex);")
+            lib = c_intf.dlopen("User32.dll") # get C funtion from this .dll
+            s_create_info = VkWin32SurfaceCreateInfoKHR(
+                hwnd = self.winId(),
+                hinstance = lib.GetWindowLongA(c_intf.cast("void*", self.winId()), -6))
+            createSurface = self.getInstanceProc("vkCreateWin32SurfaceKHR")
+        elif sys.platform == "linux":
+            VkWaylandSurfaceCreateInfoKHR()
+        self._surface = createSurface(self._instance, s_create_info, None)
 
-        d_create_info = VkDeviceCreateInfo(
-            queueCreateInfoCount = 1,
-            pQueueCreateInfos = q_create_info,
-            enabledExtensionCount = 0,
-            enabledLayerCount = 0,
-            pEnabledFeatures = VkPhysicalDeviceFeatures())
-
-        self._device = vkCreateDevice(self._physical_device, d_create_info, None)
-        # ^ NOT physical device! (distinction unknown)
-
-##        self._graphic_queue = vkGetDeviceQueue(self._device, graphics_family, 0)
-##        self._present_queue = vkGetDeviceQueue(self._device, graphics_family, 0)
+##        # physical device selection
+##        self._physical_device = None
+##        physical_devices = vkEnumeratePhysicalDevices(self._instance)
+##
+##        for device in physical_devices:
+##            family = vkGetPhysicalDeviceQueueFamilyProperties(device)
+##            
+##            for graphics_family, prop in enumerate(family):
+##                if prop.queueCount > 0:
+##                    if prop.queueFlags & VK_QUEUE_GRAPHICS_BIT:
+##                        self._physical_device = device
+##                    surface_supported = self.getInstanceProc("vkGetPhysicalDeviceSurfaceSupportKHR")
+##                    
+##                    if surface_supported(device, graphics_family, self._surface):
+##                        ...:
+##
+##        if self._physical_device == None:
+##            raise RuntimeError("Could not find suitable physical device")
+##
+##        # logical device creation
+##        q_create_infos = []
+##        for ? in ?:
+##            o = VkDeviceQueueCreateInfo(
+##                queueFamilyIndex = graphics_family,
+##                queueCount = 1,
+##                pQueuePriorities = [1.0])
+##
+##        d_create_info = VkDeviceCreateInfo(
+##            queueCreateInfoCount = len(q_create_infos),
+##            pQueueCreateInfos = q_create_infos,
+##            enabledExtensionCount = 0,
+##            enabledLayerCount = 0, # validation layers
+##            pEnabledFeatures = VkPhysicalDeviceFeatures())
+##
+##        self._device = vkCreateDevice(self._physical_device, d_create_info, None)
 
     def __del__(self):
+##        if self._surface != None:
+##            vkDestroySurfaceKHR(self._instance, self._surface, None)
+##        if self._device != None:
+##            vkDestroyDevice(self._device, None)
         if self._instance != None:
             vkDestroyInstance(self._instance, None)
+
+    def getInstanceProc(self, function_name): # returns function
+        return vkGetInstanceProcAddr(self._instance, function_name)
 
 
 if __name__ == "__main__":
@@ -78,7 +104,7 @@ if __name__ == "__main__":
     
     # APPLICATION
     app = QtWidgets.QApplication([])
-    window = VulkanWindow() # not executing?
+    window = VulkanWindow()
     window.show()
 
     def cleanup():
