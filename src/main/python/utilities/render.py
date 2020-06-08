cdaf9cca129da8c48b1285162a2f850b5eca2056
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-06-08 13:20:38 +1000

    reworking mapping of data stored in GL buffers

--- src/main/python/utilities/render.py
+++ src/main/python/utilities/render.py
@@ -66,47 +66,6 @@ def draw_ray(origin, direction, distance=4096):
     glVertex(*(origin + direction * distance))
     glEnd()
 
-# BUFFER TOOLS
-# These two funtions work to find free segments to assign to
-# render.manager method find_gap uses these functions...
-#  - however! it also checks the types of neighbours to optimise draw calls
-def compress(spans):
-    """Combine a series of spans into as few (start, length) spans as possible
-    This is to minimise calls read, write & draw calls to OpenGL buffers"""
-    spans = list(sorted(spans, key=lambda x: x[0]))
-    start, length = spans[0]
-    prev_end = start + length
-    out = []
-    current = [start, length]
-    for start, length in spans[1:]:
-        end = start + length
-        if start <= prev_end:
-            current[1] = end - current[0]
-        else:
-            out.append(tuple(current))
-            current = [start, length]
-        prev_end = end
-    out.append(tuple(current))
-    return out
-
-def free(spans, span_to_remove): # can be used to hide selection
-    """remove a (start, length) span from a series of spans"""
-    r_start, r_length = span_to_remove # span_to_remove is R
-    r_end = r_start + r_length
-    out = [] # all spans which do not intersect R
-    for start, length in spans: # current span
-        end = start + length # current end
-        if r_start <= start < end <= r_end:
-            continue # R eclipses current span
-        else:
-            if start < r_start:
-                new_end = min([end, r_start])
-                out.append((start, new_end - start)) # segment before R
-            if r_end < end:
-                new_start = max([start, r_end])
-                out.append((new_start, end - new_start)) # segment after R
-    return out
-
 
 class manager:
     """Manages OpenGL buffers and gives handles for rendering & hiding objects"""
@@ -120,6 +79,7 @@ class manager:
         self.memory_limit = 512 * MB # defined in settings
         # ^ can't check the physical device limit until after init_GL is called
 
+        # queue for self.update_buffers()
         self.buffer_update_queue = []
         # [{"type": "brush" or "displacement"
         #   "id": brush.id or brush.side[?].id
@@ -134,16 +94,16 @@ class manager:
         #                "index":  (start, length)}
         # where: renderable = {"type": "brush", "id": brush.id}
 
-
-        self.buffer_type_spans = {"vertex": {"brush": [], "displacement": [], "model": []},
-                                    "index": {"brush": [], "displacement": [], "model": []}}
+        self.buffer_allocation_map = {"vertex": {"brush": [],
+                                                 "displacement": [],
+                                                 "model": []},
+                                      "index": {"brush": [],
+                                                "displacement": [],
+                                                "model": []}}
         # ^ buffer: {type: [(start, length)]}
-        # update:
-        # self.abstract_buffer_map = ...
-        # compress([*self.abstract_buffer_map["brushes"], *adding_span])
-        # free(self.abstract_buffer_map["brushes"], deleting_span)
-        self.hidden = {"brush": [], "displacement": []}
+        self.hidden = {"brush": [], "displacement": [], "model": []}
         # ^ type: (start, length)
+        # DOES NOT YET AFFECT RENDER
 
     def init_GL(self, ctx): # called by parent viewport's initializeGL()
         glClearColor(0, 0, 0, 0)
@@ -188,12 +148,13 @@ class manager:
                     self.uniform[style][target]["matrix"] = glGetUniformLocation(self.shader[style][target], "ModelViewProjectionMatrix")
             glUseProgram(0)
         # https://github.com/snake-biscuits/QtPyHammer/wiki/Rendering:-Vertex-Format
+        self.vertex_format_size = 44
         glEnableVertexAttribArray(0) # vertex_position
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 44, GLvoidp(0))
         glEnableVertexAttribArray(1) # vertex_normal (brush only)
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, 44, GLvoidp(12))
         # glEnableVertexAttribArray(4) # blend_alpha (displacement only)
-        glVertexAttribPointer(4, 1, GL_FLOAT, GL_FALSE, 44, GLvoidp(12))
+        # glVertexAttribPointer(4, 1, GL_FLOAT, GL_FALSE, 44, GLvoidp(12))
         # ^ replaces vertex_normal if displacement
         glEnableVertexAttribArray(2) # vertex_uv
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 44, GLvoidp(24))
@@ -229,9 +190,6 @@ class manager:
             start, length, data = update["index"]
             index_span = (start, length)
             self.buffer_location[renderable] = {"vertex": vertex_span, "index": index_span}
-
-
-
         # update view matrix
         glUseProgram(self.shader[self.render_mode]["brush"])
         for uniform, location in self.uniform[self.render_mode]["brush"].items():
@@ -239,6 +197,66 @@ class manager:
                 glUniformMatrix4fv(location, 1, GL_FALSE, matrix)
 
     # BUFFER UPDATES
+    def update_buffers(self, update):
+        """update data in buffers and draw calls"""
+        # {"type": "brush" or "displacement" or model
+        #  "ids": [id, ...],
+        #  "spans": [{"vertex": (start, length),
+        #             "index": (start, length)}, ...]
+        #  "vertex": (start, length, data),
+        #  "index": (start, length, data)}
+        # ^ zip together
+        glBufferSubData(GL_ARRAY_BUFFER, *update["vertex"])
+        glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, *update["index"])
+        ids, spans = update["ids"], update["spans"]
+        for renderable_id, span in zip(ids, spans):
+            # span = {"vertex": (start, length), "index": (start, length)}
+            self.buffer_location[identifier] = span
+        self.track_span("vertex", "brush", update["vertex"][:2])
+        self.track_span("index", "brush", update["index"][:2])
+
+    # modify self.buffer_allocation_map
+    # WORKING
+    def track_span(self, buffer, renderable_type, span_to_track):
+        start, length = span_to_track
+        end = start + length
+        target = self.buffer_allocation_map[buffer][renderable_type]
+        for i, span in enumerate(target):
+            S, L = span
+            E = S + L
+            # we can safely assume no spans overlap
+            # self.find_gaps checks that
+            # but self.find_gaps doesn't update the map
+            # we don't update the map until the buffer is updated
+            if S < end and E < start:
+                continue # (S, L) is before span_to_track and doesn't touch it
+            elif S == end: # span_to_track leads (S, L)
+                old = target.pop(i)
+                target.insert(i, (S, L + length))
+            elif E == start: # span_to_track tails (S, L) 
+                S, L = target.pop(i)
+                target.insert(i, (S, L + length))
+            elif S > end:
+                break # past the end of span_to_track [we're done!]
+
+    def untrack_span(self, buffer, renderable_type, span_to_untrack):
+        r_start, r_length = span_to_remove # span_to_remove is R
+        r_end = r_start + r_length
+        out = [] # all spans which do not intersect R
+        for start, length in spans: # current span
+            end = start + length # current end
+            if r_start <= start < end <= r_end:
+                continue # R eclipses current span
+            else:
+                if start < r_start:
+                    new_end = min([end, r_start])
+                    out.append((start, new_end - start)) # segment before R
+                if r_end < end:
+                    new_start = max([start, r_end])
+                    out.append((new_start, end - new_start)) # segment after R
+        return out
+    # WORKING END
+
     def find_gaps(self, buffer="vertex", preferred_type=None, minimum_size=1):
         """Generator which yeilds a (start, length) span for each gap which meets requirements"""
         if minimum_size < 1:
@@ -279,103 +297,24 @@ class manager:
             if preferred_type in (None, span_type[prev_span]):
                     yield gap
 
-
     def add_brushes(self, *brushes):
         """Add *brushes to the appropriate GPU buffers"""
-        # the most important case is loading a fresh file which should not be overly expensive as we should have a long stretch of free memory
-        # a loading bar for long updates would be handy
-        # do long / large updates hang on the UI thread?
-        # it would be nice if we could do all this asyncronously:
-        # -- double buffering and/or using a separate thread, awaits etc.
-        brushes = iter(brushes)
-        vertex_writes = {} # {(start, length): data}
-        offset_indices = {} # {brush: indices + offset}
+        # aiming to be a generator
+        # until async is implemented
         gaps = self.find_gaps(buffer="vertex") # generator
-        allocating_brushes = True
-        brush = next(brushes)
-        vertices_size = len(brush.vertices) * 44 # 44 bytes per vertex
-        while allocating_brushes:
-            try:
-                gap_start, gap_length = next(gaps) # NEW GAP
-                allocation_start = gap_start # gap must start on a multiple of 44
-                allocated_length = 0
-                data = [] # [*brush.vertices]
-            except StopIteration: # out of gaps
-                allocating_brushes = False
-                continue # quit loop
-            while vertices_size < gap_length: # CHECK BRUSH
-                data += brush.vertices # add to write list
-                self.buffer_map["vertex"][brush] = (gap_start, vertices_size)
-                # add offset to all indicies for later
-                offset = lambda i: i + (gap_start // 44)
-                offset_indices[brush] = list(map(offset, brush.indices))
-                # update gap related variables
-                allocated_length += vertices_size
-                gap_start += vertices_size
-                gap_length -= vertices_size
-                try:
-                    brush = next(brushes)
-                    vertices_size = len(brush.vertices) * 44 # bytes per vertex
-                except StopIteration: # out of brushes
-                    allocating_brushes = False # FINAL WRITE
-                    # DO NOT CONTINUE! WE STILL NEED TO WRITE!
-            if allocated_length > 0: # we have data to write!
-                vertex_writes[(allocation_start, allocated_length)] = data
-        # check we don't have any brushes left over, if we do:
-        # - check there is enough VRAM to fit the leftovers into
-        # - raise an error if we are out of VRAM
-        # - squeeze our leftovers into ANY gaps (no find_gaps preference)
-
-        vertex_spans = [*self.abstract_buffer_map["vertex"]["brush"],
-                        *vertex_writes.keys()]
-        self.abstract_buffer_map["vertex"]["brush"] = compress(vertex_spans)
-        # ^ update abstract_buffer_map with all the brushes we have prepared
-
-        # VERTEX_BUFFER WRITES
-        self.queued_updates.append(
-            (glBindBuffer, *(GL_ARRAY_BUFFER, self.VERTEX_BUFFER)))
-        for span, data in vertex_writes.items():
-            start, length = span
-            data = list(itertools.chain(*data))
-            data = np.array(data, dtype=np.float32)
-            self.queued_updates.append(
-                (glBufferSubData, *(GL_ARRAY_BUFFER, start, length, data)))
-        del vertex_writes
-
-        # ^^ offset_indices = {brush: indices + offset} ^^
-        index_writes = {} # {(start, length): data}
-        for gap in self.find_gaps(buffer="index", preferred_type="brush"):
-            gap_start, gap_length = gap
-            true_gap_start = gap_start
-            gap_sufficient = True
-            data = [] # itertools.chain([brush.indices + offset])
-            while gap_sufficient and len(offset_indices) > 0:
-                brush, indices = list(offset_indices.items())[0]
-                indices_size = len(indices) * 4 # sizeof(np.uint32)
-                if indices_size > gap_length:
-                    gap_sufficient = False
-                    continue # go to next gap
-                # implied else
-                self.buffer_map["index"][brush] = (gap_start, indices_size)
-                data.append(indices)
-                gap_start += indices_size
-                gap_length -= indices_size
-                offset_indices.pop(brush) # data for brush has been allocated
-            index_writes[(true_gap_start, len(data) * 4)] = data
-        if len(offset_indices) > 0: # lazy method couldn't pack every brush
-            gaps = [g for g in self.find_gaps(buffer="index")]
-            # check total_gap_length < remaining_data_length
-        index_spans = [*self.abstract_buffer_map["index"]["brush"],
-                       *index_writes.keys()]
-        self.abstract_buffer_map["index"]["brush"] = compress(index_spans)
-
-        # INDEX_BUFFER WRITES
-        self.queued_updates.append(
-            (glBindBuffer, *(GL_ELEMENT_ARRAY_BUFFER, self.INDEX_BUFFER)))
-        for span, data in index_writes.items():
-            start, length = span
-            data = list(itertools.chain(*data))
-            data = np.array(data, dtype=np.uint32)
-            self.queued_updates.append(
-                (glBufferSubData, *(GL_ELEMENT_ARRAY_BUFFER, start, length, data)))
-        del index_writes
+        brushes = iter(brushes)
+        def next_brush():
+            out = {"brush": next(brushes),
+                   "identifier": {"type": "brush", "id": brush.id}}
+            flat_vertex_data = itertools.chain(*brush.vertices)
+            out["vertex_data"] = np.array(flat_vertex_data, dtype=np.float32)
+            out["vertex_length"] = len(brush.vertices) * self.vertex_format_size
+            out["index_data"] = np.array(brush.indices, dtype=np.uint32)
+            out["index_length"] = len(brush.indices) * 4
+            return out
+        brush = next_brush()
+        allocating = True
+        for gap in gaps:
+            brush ...
+            # yield an buffer_update for the queue
+            # -- for each gap filled
