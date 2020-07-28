e35309b66265596204798df44e5cfb79537b4961
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-07-28 21:50:42 +1000

    added a dark mode palette to test_run.py

--- test_run.py
+++ test_run.py
@@ -10,6 +10,32 @@ def except_hook(cls, exception, traceback): # for debugging Qt slots
 sys.excepthook = except_hook
 
 app = QtWidgets.QApplication([])
+# DARK MODE
+app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
+palette = QtGui.QPalette
+dark_palette = palette()
+base = QtGui.QColor(45, 45, 45)
+locked = QtGui.QColor(127, 127, 127)
+dark_palette.setColor(palette.Window, base)
+dark_palette.setColor(palette.WindowText, QtCore.Qt.white)
+dark_palette.setColor(palette.Base, QtGui.QColor(18,18,18))
+dark_palette.setColor(palette.AlternateBase, base)
+dark_palette.setColor(palette.ToolTipBase, QtCore.Qt.white)
+dark_palette.setColor(palette.ToolTipText, QtCore.Qt.white)
+dark_palette.setColor(palette.Text, QtCore.Qt.white);
+dark_palette.setColor(palette.Disabled, palette.Text, locked)
+dark_palette.setColor(palette.Button, base)
+dark_palette.setColor(palette.ButtonText, QtCore.Qt.white)
+dark_palette.setColor(palette.Disabled, palette.ButtonText, locked)
+dark_palette.setColor(palette.BrightText, QtCore.Qt.red)
+dark_palette.setColor(palette.Link, QtGui.QColor(42, 130, 218))
+dark_palette.setColor(palette.Highlight, QtGui.QColor(42, 130, 218))
+dark_palette.setColor(palette.HighlightedText, QtCore.Qt.black)
+dark_palette.setColor(palette.Disabled, palette.HighlightedText, locked)
+# qpalette.light, midlight, dark, mid, shadow
+app.setPalette(dark_palette)
+app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+
 # TEST CODE
 window = MainWindow()
 window.showMaximized()
