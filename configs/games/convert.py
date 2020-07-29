b788cefe02ef94952d72c71127939207b1430545
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-07-29 19:53:03 +1000

    generated .ini files from GameConfig.txt

--- configs/games/convert.py
+++ configs/games/convert.py
@@ -1,9 +1,9 @@
 import re
 
-from Qt import QtCore
+from PyQt5 import QtCore
 
 
-config_txt = open("Team Fortress 2/bin/GameConfig.txt")
+config_txt = open("../../Team Fortress 2/bin/GameConfig.txt")
 # Configs > Games > Half-Life 2 > GameDir
 #                                 Hammer > GameData0 ... MapDir ...
 # Configs > Games > Team Fortress 2 > ...
@@ -11,18 +11,20 @@ context = ""
 depth = 0
 prev_line = ""
 for line in config_txt:
-    if depth == 3:
-        active_ini = QtCore.QSettings(f"{context}.ini", QtCore.QSettings.IniFormat)
-    elif depth > 3:
-        active_ini.beginGroup(context)
     if '{' in line:
-        depth += 1  
+        depth += 1
+        if depth == 3:
+            active_ini = QtCore.QSettings(f"{context}.ini", QtCore.QSettings.IniFormat)
+        elif depth > 3:
+            active_ini.beginGroup(context)
     elif '}' in line:
         depth -= 1
+        if depth == 3:
+            active_ini = QtCore.QSettings(f"{context}.ini", QtCore.QSettings.IniFormat)
     elif line.count('"') == 2: # e.g. "Games"
-        context = re.search('^\ *"[^"]+"\ *$', line).group(0)
-        # ^ contents of quotes
+        context = re.search('(?<=").*(?=")', line).group(0)
     elif line.count('"') == 4 and depth >= 3: # Key-Value pair
-        key, value = re.findall('(?<=")[^"]+?(?=")')
+        key, value = re.findall('(?<=")[^"\t]+?(?=")', line)
+        value = value.replace("\\", "/")
         active_ini.setValue(key, value)
     prev_line = line
