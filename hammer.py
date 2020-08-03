26b77de90947bbe13fcec5b9c379d2b90bd2aa97
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-08-03 14:50:03 +1000

    updated hammer.py to work with current configs

--- hammer.py
+++ hammer.py
@@ -16,8 +16,8 @@ app = QtWidgets.QApplication(sys.argv)
 # ^ app can be accessed from anywhere with QtWidgets.QApplication.instance()
 
 # load all config files
-preferences = QtCore.QSettings("configs/preferences.ini", QtCore.QSettings.IniFormat)
-game = preferences.value("Game", "Team Fortress 2")
+app.preferences = QtCore.QSettings("configs/preferences.ini", QtCore.QSettings.IniFormat)
+game = app.preferences.value("Game", "Team Fortress 2")
 app.game_config = QtCore.QSettings(f"configs/games/{game}.ini", QtCore.QSettings.IniFormat)
 app.hotkeys = QtCore.QSettings("configs/hotkeys.ini", QtCore.QSettings.IniFormat)
 
@@ -25,25 +25,26 @@ app.themes = dict()
 for filename in os.listdir("configs/themes/"):
     theme_name = filename.rpartition(".")[0] # filename without extention
     app.themes[theme_name] = load_theme(f"configs/themes/{filename}")
-theme = preferences.value("Theme", "light_mode")
+theme = app.preferences.value("Theme", "default")
+if theme not in app.themes:
+    theme = "default"
 app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
 app.setPalette(app.themes[theme])
 
 if sys.platform == "win32":
     reg = QtCore.QSettings("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", QtCore.QSettings.NativeFormat)
-    print(reg.value("AppsUseLightTheme"))
     if reg.value("AppsUseLightTheme") == 0:
-        app.setPalette(app.themes["dark_mode"])
+        dark_theme = f"{theme}_dark"
+        if dark_theme not in app.themes:
+            dark_theme = "default_dark"
+        app.setPalette(app.themes[dark_theme])
         app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
         # ^ allow themes to include .css files
         # -- will need some way to indicate which widgets have them set
         
 # load all entities from the .fgd
 fgd_file = app.game_config.value("Hammer/GameData0") # the .fgd
-fgd = fgdtools.parser.FgdParse(fgd_file)
-app.entities = list(fgd.entities)
-for included_fgd in fgd.includes:
-    app.entities.extend(included_fgd.entities)
+app.fgd = fgdtools.parser.FgdParse(fgd_file)
 
 # check gameinfo.txt for extra content paths
 # allow the user to load custom content from their own folders
