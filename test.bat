32c81d6bc19aa9d15826e8db9495056da2253860
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-09-02 16:51:03 +1000

    simplified test boot scripts

--- test.bat
+++ test.bat
@@ -1,2 +1,1 @@
-call venv/scripts/activate
 python hammer.py "Team Fortress 2/tf/mapsrc/%1.vmf"
--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-source venv/bin/activate
 python hammer.py "Team Fortress 2/tf/mapsrc/${1}.vmf"
