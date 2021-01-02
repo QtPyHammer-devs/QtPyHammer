41fce17425eb28a1269cb12d3e14be58ddaaa502
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-01-03 09:01:09 +1000

    preparing for new renderable classes for better selective rendering Hiding objects + selection mask highlighting

--- QtPyHammer/utilities/render/__init__.py
+++ QtPyHammer/utilities/render/__init__.py
@@ -1,3 +1,24 @@
-__all__ = ["Manager"]
+from __future__ import annotations
 
+__all__ = ["Manager"]
 from .manager import Manager
+
+
+class Span:
+    def __init__(self, spans=list()):
+        self._spans = spans
+
+    def __add__(self, other: Span):
+        ...
+
+
+class Renderable:
+    def __init__(self, editor_object):
+        self.uuid = (object.__class__.__name__, editor_object.id)
+        self.children = [(*self.uuid, c.id) for c in editor_object]
+        self.buffer_map = {c.id: {"vertex": [0, 0], "index": [0, 0]} for c in self.children}
+
+    @staticmethod
+    def set_gl_state():
+        """Set all GL state required for this renderable type"""
+        ...
