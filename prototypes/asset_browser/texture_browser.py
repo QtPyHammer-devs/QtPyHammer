c766e9483347cd787e40f743afec130674c6b0ff
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-01-02 22:10:54 +1000

    pep8 stuff
    Co-Authored-By: null <asd417@users.noreply.github.com>

--- prototypes/asset_browser/texture_browser.py
+++ prototypes/asset_browser/texture_browser.py
@@ -5,13 +5,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class TextureBrowser(QtWidgets.QDialog):
     # https://doc.qt.io/qt-5/qdialog.html
-    def __init__(self):
-        super(TextureBrowser, self).__init__(parent=None)
+    def __init__(self, parent=None):
+        super().__init__(parent)
         # pick a layout for the core widget
         # top has a scrolling page of texture thumbnails
-        # bottom has filters, searchbar, OK & Cancel buttons
+        # bottom has filters, self.searchbar, OK & Cancel buttons
 
-        outerQV = QtWidgets.QVBoxLayout()
+        main_layout = QtWidgets.QVBoxLayout()
 
         # now this has scroll bar but it doesnt have flow layout
         scroll = QtWidgets.QScrollArea()
@@ -41,27 +41,29 @@ class TextureBrowser(QtWidgets.QDialog):
         scroll.setWidgetResizable(True)
 
         # finally add scrollarea widget to the outer QVBoxLayout
-        outerQV.addWidget(scroll)
-        outerQV.addWidget(QtWidgets.QLabel("Search Options"))  # placeholder
+        main_layout.addWidget(scroll)
+        main_layout.addWidget(QtWidgets.QLabel("Search Options"))
+        # list of checkboxes?
+        # colour-range (hue) slider (.vtf reflectivity value)
 
-        searchbar = QtWidgets.QLineEdit()
-        outerQV.addWidget(searchbar)
+        self.searchbar = QtWidgets.QLineEdit()
+        main_layout.addWidget(self.searchbar)
 
-        searchButton = QtWidgets.QPushButton("Search")
-        searchButton.clicked.connect(lambda: self.search(searchbar.text()))
-        searchButton.setDefault(1)
+        search_button = QtWidgets.QPushButton("Search")
+        search_button.clicked.connect(lambda: self.search(self.searchbar.text()))
+        search_button.setDefault(1)
 
-        outerQV.addWidget(searchButton)
+        main_layout.addWidget(search_button)
 
         buttonbox = QtWidgets.QDialogButtonBox
         buttons = buttonbox(buttonbox.Ok | buttonbox.Cancel)
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
-        outerQV.addWidget(buttons)
-        self.setLayout(outerQV)
+        main_layout.addWidget(buttons)
+        self.setLayout(main_layout)
 
     def addTextureSquare(self, layout):
-        imagelabel = QtWidgets.QLabel()
+        image_label = QtWidgets.QLabel()
         # Placeholder Image
         magenta = b"\xFF\x00\xFF"
         black = b"\x00\x00\x00"
@@ -70,9 +72,9 @@ class TextureBrowser(QtWidgets.QDialog):
         # Parse data into image
         image = QtGui.QImage(data, 2, 2, QtGui.QImage.Format_RGB888)
         image.setDevicePixelRatio(1 / 32)  # scale *32
-        imagelabel.setPixmap(QtGui.QPixmap.fromImage(image))
+        image_label.setPixmap(QtGui.QPixmap.fromImage(image))
 
-        layout.addWidget(imagelabel)
+        layout.addWidget(image_label)
 
     def search(self, keyword):
         print(f"Trying to Search {keyword}!")
@@ -99,43 +101,43 @@ class FlowLayout(QtWidgets.QLayout):
         while self.count():
             self.takeAt(0)
 
-    def addItem(self, item):  # pylint: disable=invalid-name
+    def addItem(self, item):
         self._item_list.append(item)
 
-    def addSpacing(self, size):  # pylint: disable=invalid-name
+    def addSpacing(self, size):
         self.addItem(QtWidgets.QSpacerItem(size, 0, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum))
 
     def count(self):
         return len(self._item_list)
 
-    def itemAt(self, index):  # pylint: disable=invalid-name
+    def itemAt(self, index):
         if 0 <= index < len(self._item_list):
             return self._item_list[index]
         return None
 
-    def takeAt(self, index):  # pylint: disable=invalid-name
+    def takeAt(self, index):
         if 0 <= index < len(self._item_list):
             return self._item_list.pop(index)
         return None
 
-    def expandingDirections(self):  # pylint: disable=invalid-name,no-self-use
+    def expandingDirections(self):
         return QtCore.Qt.Orientations(QtCore.Qt.Orientation(0))
 
-    def hasHeightForWidth(self):  # pylint: disable=invalid-name,no-self-use
+    def hasHeightForWidth(self):
         return True
 
-    def heightForWidth(self, width):  # pylint: disable=invalid-name
+    def heightForWidth(self, width):
         height = self._do_layout(QtCore.QRect(0, 0, width, 0), True)
         return height
 
-    def setGeometry(self, rect):  # pylint: disable=invalid-name
+    def setGeometry(self, rect):
         super().setGeometry(rect)
         self._do_layout(rect, False)
 
-    def sizeHint(self):  # pylint: disable=invalid-name
+    def sizeHint(self):
         return self.minimumSize()
 
-    def minimumSize(self):  # pylint: disable=invalid-name
+    def minimumSize(self):
         size = QtCore.QSize()
 
         for item in self._item_list:
@@ -160,10 +162,10 @@ class FlowLayout(QtWidgets.QLayout):
             space_x = self.spacing()
             space_y = self.spacing()
             if wid is not None:
-                space_x += wid.style().layoutSpacing(
-                    QtWidgets.QSizePolicy.PushButton, QtWidgets.QSizePolicy.PushButton, QtCore.Qt.Horizontal)
-                space_y += wid.style().layoutSpacing(
-                    QtWidgets.QSizePolicy.PushButton, QtWidgets.QSizePolicy.PushButton, QtCore.Qt.Vertical)
+                space_x += wid.style().layoutSpacing(QtWidgets.QSizePolicy.PushButton,
+                                                     QtWidgets.QSizePolicy.PushButton, QtCore.Qt.Horizontal)
+                space_y += wid.style().layoutSpacing(QtWidgets.QSizePolicy.PushButton,
+                                                     QtWidgets.QSizePolicy.PushButton, QtCore.Qt.Vertical)
 
             next_x = x + item.sizeHint().width() + space_x
             if next_x - space_x > effective_rect.right() and line_height > 0:
@@ -183,15 +185,15 @@ class FlowLayout(QtWidgets.QLayout):
         return new_height
 
 
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
+if __name__ == "__main__":
+    def except_hook(cls, exception, traceback):
+        """Get tracebacks for python called by Qt functions & classes"""
+        sys.__excepthook__(cls, exception, traceback)
 
+    sys.excepthook = except_hook
+    app = QtWidgets.QApplication(sys.argv)
+    browser = TextureBrowser()
+    browser.setGeometry(128, 64, 576, 576)
+    browser.show()
 
-sys.excepthook = except_hook  # For debugging python inside Qt Classes
-
-app = QtWidgets.QApplication(sys.argv)
-window = TextureBrowser()
-window.setGeometry(128, 64, 576, 576)
-window.show()
-
-app.exec_()
+    app.exec_()
