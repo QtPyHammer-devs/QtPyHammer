d9813b560b6930155fcc1e6967d5850ce35323df
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-01-04 18:59:42 +1000

    namedtuple for renderable UUIDs

--- QtPyHammer/utilities/render/__init__.py
+++ QtPyHammer/utilities/render/__init__.py
@@ -1,22 +1,29 @@
 from __future__ import annotations
 
-__all__ = ["Manager"]
+__all__ = ["Manager", "Span", "Renderable", "RenderableId", "RenderableChildId"]
+import collections
+
 from .manager import Manager
 
 
 class Span:
     def __init__(self, spans=list()):
-        self._spans = spans
+        self.start, self.end
 
     def __add__(self, other: Span):
         ...
 
 
+RenderableId = collections.namedtuple("RenderableId", ["type", "id"])
+RenderableChildId = collections.namedtuple("RenderableChildId", ["type", "id", "child_id"])
+
+
 class Renderable:
     def __init__(self, editor_object):
-        self.uuid = (object.__class__.__name__, editor_object.id)
-        self.children = [(*self.uuid, c.id) for c in editor_object]
-        self.buffer_map = {c.id: {"vertex": [0, 0], "index": [0, 0]} for c in self.children}
+        self.uuid = RenderableId(object.__class__.__name__, editor_object.id)
+        self.children = [RenderableChildId(*self.uuid, c.id) for c in editor_object]
+        # ^ editor_object.__iter__() must return children with .id attrs, or None
+        self.buffer_map = {c.child_id: {"vertex": Span(), "index": Span()} for c in self.children}
 
     @staticmethod
     def set_gl_state():
