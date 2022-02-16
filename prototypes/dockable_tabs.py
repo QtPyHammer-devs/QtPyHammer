63542c9a4cf64963c90b302f0f625323b81cdec2
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2022-02-16 16:14:41 +1000

    single quotes irk me

--- prototypes/dockable_tabs.py
+++ prototypes/dockable_tabs.py
@@ -6,6 +6,8 @@ from PyQt5 import QtCore, QtWidgets
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
+
+
 sys.excepthook = except_hook # Python Debug
 
 
@@ -16,9 +18,9 @@ center = QtWidgets.QWidget()
 center.setBaseSize(256, 256)
 window.setCentralWidget(center)
 layout = QtWidgets.QGridLayout()
-dock_01 = QtWidgets.QDockWidget('Tab 1')
-dock_02 = QtWidgets.QDockWidget('Tab 2')
-dock_03 = QtWidgets.QDockWidget('Tab 3')
+dock_01 = QtWidgets.QDockWidget("Tab 1")
+dock_02 = QtWidgets.QDockWidget("Tab 2")
+dock_03 = QtWidgets.QDockWidget("Tab 3")
 window.setTabPosition(QtCore.Qt.TopDockWidgetArea, QtWidgets.QTabWidget.North)
 window.addDockWidget(QtCore.Qt.TopDockWidgetArea, dock_01, QtCore.Qt.Vertical)
 window.tabifyDockWidget(dock_01, dock_02)
